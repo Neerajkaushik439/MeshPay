5f606638ce26266ff54eb11dbc026e2a2a3595d9
Neeraj <neerajkaushik439@gmail.com>
2026-09-06 12:23:53 +0530

    added SQL query in microservive to keep db active

--- transaction-service/src/main/java/com/meshpay/transactionservice/controller/HealthController.java
+++ transaction-service/src/main/java/com/meshpay/transactionservice/controller/HealthController.java
@@ -1,12 +1,18 @@
 package com.meshpay.transactionservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class HealthController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/health")
     public String health() {
-        return "Transaction Service Running";
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+        return "Transaction Service Running and DB OK";
     }
 }
